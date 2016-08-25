f82412606f23c9c1beff0bf3d9b42a8e2228776d
Chris Viccaro <cviccaro@jpenterprises.com>
2016-08-25 14:39:54 -0400

    Update to project.config to explicitly add angular2-material packages due to strange systemjs build failure on windows

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -22,7 +22,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      { src: 'jquery/dist/jquery.min.js', inject: 'libs' },
       { src: 'dynamics.js/lib/dynamics.min.js', inject: 'libs' },
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
@@ -30,7 +30,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `local` third-party libraries to be injected/bundled.
     this.APP_ASSETS = [
       ...this.APP_ASSETS,
-      { src: `${this.CSS_SRC}/mobile.${ this.getInjectableStyleExtension() }`, inject: true, vendor: false }
+      { src: `${this.CSS_SRC}/mobile.${this.getInjectableStyleExtension()}`, inject: true, vendor: false }
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
@@ -42,14 +42,91 @@ export class ProjectConfig extends SeedConfig {
     this.SYSTEM_BUILDER_CONFIG.packageConfigPaths.push(join(this.PROJECT_ROOT, 'node_modules', '@angular2-material', '*', 'package.json'));
 
     this.SYSTEM_BUILDER_CONFIG.packages['moment'] = {
-        main: 'min/moment-with-locales.min.js',
-        defaultExtension: 'js'
+      main: 'min/moment-with-locales.min.js',
+      defaultExtension: 'js'
     };
 
     this.SYSTEM_BUILDER_CONFIG.packages['angular2-moment'] = {
-        main: 'index.js',
-        defaultExtension: 'js'
+      main: 'index.js',
+      defaultExtension: 'js'
     };
+
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/core'] = {
+      main: 'core.js',
+      defaultExtension: 'js'
+    };
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/button'] = {
+      main: 'button.js',
+      defaultExtension: 'js'
+    };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/button-toggle'] = {
+    //   main: 'button-toggle.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/card'] = {
+    //   main: 'card.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/checkbox'] = {
+    //   main: 'checkbox.js',
+    //   defaultExtension: 'js'
+    // };
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/grid-list'] = {
+      main: 'grid-list.js',
+      defaultExtension: 'js'
+    };
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/icon'] = {
+      main: 'icon.js',
+      defaultExtension: 'js'
+    };
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/input'] = {
+      main: 'input.js',
+      defaultExtension: 'js'
+    };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/list'] = {
+    //    main: 'list.js',
+    //    defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/menu'] = {
+    //   main: 'menu.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/progress-bar'] = {
+    //   main: 'progress-bar.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/progress-circle'] = {
+    //   main: 'progress-circle.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/radio'] = {
+    //   main: 'radio.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/sidenav'] = {
+    //   main: 'sidenav.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/slider'] = {
+    //   main: 'slider.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/slide-toggle'] = {
+    //   main: 'slide-toggle.js',
+    //   defaultExtension: 'js'
+    // };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/tabs'] = {
+    //   main: 'tabs.js',
+    //   defaultExtension: 'js'
+    // };
+    this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/toolbar'] = {
+      main: 'toolbar.js',
+      defaultExtension: 'js'
+    };
+    // this.SYSTEM_BUILDER_CONFIG.packages['@angular2-material/tooltip'] = {
+    //   main: 'tooltip.js',
+    //   defaultExtension: 'js'
+    // };
 
     this.SYSTEM_BUILDER_CONFIG.packages['angular2-google-maps/core'] = {
       defaultExtension: 'js',
