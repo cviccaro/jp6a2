66f80e8122b74cb150a51c4c24aa509ca35e920d
Chris Viccaro <cviccaro@jpenterprises.com>
2016-09-15 15:04:29 -0400

    Updated package.json and env

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,7 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   API: 'https://api.jpedev.com',
-  desktopWidth: 960,
+  desktopWidth: 1160,
   GoogleMapsAPIKey: 'AIzaSyD6OLSQawz3GV_l25DsiOkJprC_PRO57Rc',
   ENV: 'DEV'
 };
--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,7 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
 	API: 'https://api.jpedev.com',
-	desktopWidth: 960,
+	desktopWidth: 1160,
 	GoogleMapsAPIKey: 'AIzaSyD6OLSQawz3GV_l25DsiOkJprC_PRO57Rc',
 	ENV: 'PROD'
 };
