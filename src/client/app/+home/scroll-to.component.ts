585e94af64c31e4a208d0469735171a2caca1a97
Chris Viccaro <cviccaro@jpenterprises.com>
2016-09-15 11:08:02 -0400

    Finished edits from pete

--- src/client/app/+home/scroll-to.component.ts
+++ src/client/app/+home/scroll-to.component.ts
@@ -19,13 +19,9 @@ export class ScrollToComponent implements AfterViewInit, OnDestroy {
 		this.sub = this.route.params.subscribe(params => {
 			if (params.hasOwnProperty('selector')) {
 				let el = document.getElementById(params['selector']);
-				let offset = -45;
 
-				if (params['selector'] === 'projects') {
-					offset = 0;
-				}
 				if (el) {
-					setTimeout(() => this.scrollToEl(el, offset), this.delay);
+					setTimeout(() => this.scrollToEl(el, 0), this.delay);
 				}
 			}
 		});
