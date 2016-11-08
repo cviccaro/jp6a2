7540e7e5de263cd7dcdd2bb6a94512beb1b6005b
Chris Viccaro <cviccaro@jpenterprises.com>
2016-11-08 12:15:04 -0500

    Update dev env

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -1,7 +1,7 @@
 import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
-    API: '//jpapi.localhost',
+    API: '//api.jpedev.com',
     desktopWidth: 992,
     GoogleMapsAPIKey: 'AIzaSyD6OLSQawz3GV_l25DsiOkJprC_PRO57Rc',
     GoogleRecaptchaAPIKey: '6LeUUgkUAAAAAJHKBv9tQjl9GcmQOAPO0ytXXTEI',
