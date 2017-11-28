e923e3cb759688380bf889183e2a6fae6c775da7
Travis <me@travis.com>
2017-11-28 15:40:36 -0500

    Added new tracking code for Google Analytics

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -179,8 +179,10 @@ export class SeedConfig {
   /**
    * Tracking ID.
    * @type {string}
-   */
+   
   GOOGLE_ANALYTICS_ID = 'UA-XXXXXXXX-X';
+  */
+  GOOGLE_ANALYTICS_ID = 'UA-27349841-1';
 
   /**
    * The base folder of the applications source files.
