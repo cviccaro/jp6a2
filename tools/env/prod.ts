f40d6390547fe5988de84e0db0d6eaebfbe3645f
Travis <me@travis.com>
2017-12-05 11:00:13 -0500

    Added new tracking code for Google Analytics

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -5,7 +5,7 @@ const ProdConfig: EnvConfig = {
   desktopWidth: 992,
   GoogleMapsAPIKey: 'AIzaSyBKpL7t-M7BpLeh4xc2gIsOhzYMyQJ0EeM',
   GoogleRecaptchaAPIKey: '6LccUQkUAAAAABmKw5rflMFcFo6t3m0SqfUEFBv0',
-  GoogleAnalyticsAPIKey: 'UA-26122448-1',
+  GoogleAnalyticsAPIKey: 'UA-27349841-1',
   siteTitle: 'Advertising & Digital Marketing Agency | JP Enterprises',
   ENV: 'PROD'
 };
