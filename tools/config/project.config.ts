55b3a288557e43ca19ca62b5518e9d656a0270fe
Chris Viccaro <cviccaro@jpenterprises.com>
2018-01-12 16:08:51 -0500

    Almost done with refactor

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -111,6 +111,66 @@ export class ProjectConfig extends SeedConfig {
       path: 'node_modules/@angular/cdk/bundles/cdk.umd.js'
     },
     {
+      name: '@angular/cdk/a11y',
+      path: 'node_modules/@angular/cdk/bundles/cdk-a11y.umd.js'
+    },
+    {
+      name: '@angular/cdk/accordion',
+      path: 'node_modules/@angular/cdk/bundles/cdk-accordion.umd.js'
+    },
+    {
+      name: '@angular/cdk/bidi',
+      path: 'node_modules/@angular/cdk/bundles/cdk-bidi.umd.js'
+    },
+    {
+      name: '@angular/cdk/coercion',
+      path: 'node_modules/@angular/cdk/bundles/cdk-coercion.umd.js'
+    },
+    {
+      name: '@angular/cdk/collections',
+      path: 'node_modules/@angular/cdk/bundles/cdk-collections.umd.js'
+    },
+    {
+      name: '@angular/cdk/layout',
+      path: 'node_modules/@angular/cdk/bundles/cdk-layout.umd.js'
+    },
+    {
+      name: '@angular/cdk/keycodes',
+      path: 'node_modules/@angular/cdk/bundles/cdk-keycodes.umd.js'
+    },
+    {
+      name: '@angular/cdk/observers',
+      path: 'node_modules/@angular/cdk/bundles/cdk-observers.umd.js'
+    },
+    {
+      name: '@angular/cdk/overlay',
+      path: 'node_modules/@angular/cdk/bundles/cdk-overlay.umd.js'
+    },
+    {
+      name: '@angular/cdk/platform',
+      path: 'node_modules/@angular/cdk/bundles/cdk-platform.umd.js'
+    },
+    {
+      name: '@angular/cdk/portal',
+      path: 'node_modules/@angular/cdk/bundles/cdk-portal.umd.js'
+    },
+    {
+      name: '@angular/cdk/rxjs',
+      path: 'node_modules/@angular/cdk/bundles/cdk-rxjs.umd.js'
+    },
+    {
+      name: '@angular/cdk/scrolling',
+      path: 'node_modules/@angular/cdk/bundles/cdk-scrolling.umd.js'
+    },
+    {
+      name: '@angular/cdk/stepper',
+      path: 'node_modules/@angular/cdk/bundles/cdk-stepper.umd.js'
+    },
+    {
+      name: '@angular/cdk/table',
+      path: 'node_modules/@angular/cdk/bundles/cdk-table.umd.js'
+    },
+    {
       name: '@angular/material',
       path: 'node_modules/@angular/material/bundles/material.umd.js'
     },
